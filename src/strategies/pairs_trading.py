58c369b1c196e1e6cd47f8b0e849059f7707241c
yangsu01 <ssgc1101@gmail.com>
2024-09-17 12:59:09 -0700

    WIP

---
+++ src/strategies/pairs_trading.py
@@ -0,0 +1,13 @@
+"""
+Trading strategy based on pairs trading.
+
+Objective:
+    Utilize statistical arbitrage to capture the long term 
+    mean-reverting behavior of two cointegrated stocks.
+    
+Data Used: daily historical prices of two cointegrated stocks
+Training data length: 3 years (~750 trading days)
+Trading Frequency: daily
+
+Steps:
+"""
