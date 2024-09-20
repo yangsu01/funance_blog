a27e31d0132981cb7c7d09a81d26a36a10f4da00
yangsu01 <ssgc1101@gmail.com>
2024-09-20 16:57:23 -0700

    Reword descriptions and comments

--- src/backtest/backtest_trader.py
+++ src/backtest/backtest_trader.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import gmean
+from typing import Tuple, List, Dict
 
 from ..strategies import Strategy
 from ..utils import plot_time_series, plot_returns_distribution, get_risk_free_rate
@@ -36,7 +37,7 @@ class BacktestTrader:
         self.bootstrap_results = pd.DataFrame(index=['mean', 'median', 'std', '95 upper', '95 lower'])
   
                      
-    def _calculate_performance(self, price_data: pd.Series):
+    def _calculate_performance(self, price_data: pd.Series) -> Tuple[pd.Series, pd.Series]:
         signals = self.strategy.generate_signals(price_data)
         
         cash = self.starting_cash
@@ -89,7 +90,13 @@ class BacktestTrader:
         return portfolio_values, portfolio_returns
 
 
-    def _stationary_bootstrap(self, initial_price: float, returns_data: pd.Series, n_iterations: int, block_size: int):
+    def _stationary_bootstrap(
+        self, 
+        initial_price: float, 
+        returns_data: pd.Series, 
+        n_iterations: int, 
+        block_size: int
+    ) -> List[List[float]]:
         data_len = len(returns_data)
         bootstrap_samples = []
         
@@ -113,7 +120,7 @@ class BacktestTrader:
         return bootstrap_samples
 
 
-    def _calculate_stats(self, returns_data: pd.Series):
+    def _calculate_stats(self, returns_data: pd.Series) -> Dict[str, float]:
         average_returns = gmean(np.array(returns_data)+1)-1
         risk_free_rate = get_risk_free_rate(self.data.index[0], self.data.index[-1])
         
@@ -140,18 +147,29 @@ class BacktestTrader:
         }
         
     
-    def _check_backtest_ran(self):
+    def _check_backtest_ran(self) -> None:
         if len(self.portfolio_value) == 0:
             raise ValueError('First run the backtest using run_backtest()')
         
         
-    def run_backtest(self):
+    def run_backtest(self) -> None:
+        """ Runs the backtest using the strategy and data provided
+        """
         values, returns = self._calculate_performance(self.data)
         self.portfolio_value = values
         self.portfolio_returns = returns
         
         
-    def run_bootstrap(self, n_iterations: int=1000, block_size: int=10):
+    def run_bootstrap(self, n_iterations: int=1000, block_size: int=10) -> pd.DataFrame:
+        """ Runs a stationary bootstrap on the backtest results
+
+        Args:
+            n_iterations (int, optional): Number of bootstrap iterations. Defaults to 1000.
+            block_size (int, optional): Expected length of bootstrap block. Defaults to 10.
+
+        Returns:
+            pd.DataFrame: dataframe of statistics from the bootstrap
+        """
         initial_price = self.data.iloc[0]
         returns = self.data.pct_change(fill_method=None).dropna()
         samples = self._stationary_bootstrap(initial_price, returns, n_iterations, block_size)
@@ -177,7 +195,12 @@ class BacktestTrader:
         return self.bootstrap_results.T
 
 
-    def get_results(self):
+    def get_results(self) -> Dict[str, float]:
+        """ Gets the summary of backtest results
+
+        Returns:
+            Dict[str, float]: backtest results
+        """
         self._check_backtest_ran
         
         stats = self._calculate_stats(self.portfolio_returns)
@@ -187,12 +210,23 @@ class BacktestTrader:
         return stats
     
     
-    def get_data(self):
+    def get_data(self) -> Tuple[pd.Series, pd.Series]:
+        """ Gets the backtested portfolio value and returns
+
+        Returns:
+            Tuple[pd.Series, pd.Series]: Backtested portfolio value and returns
+        """
+        
         self._check_backtest_ran
         return self.portfolio_value, self.portfolio_returns
         
 
-    def plot_results(self, title: str='Backtest Results'):
+    def plot_results(self, title: str='Backtest Results') -> None:
+        """ Plots the time series of backtested portfolio value
+
+        Args:
+            title (str, optional): title of plot. Defaults to 'Backtest Results'.
+        """
         self._check_backtest_ran
         plot_time_series(
             self.portfolio_value,
@@ -201,7 +235,13 @@ class BacktestTrader:
         )
         
     
-    def plot_analysis(self, title: str='Backtest Returns Distribution', hist_bins: int=50):
+    def plot_analysis(self, title: str='Backtest Returns Distribution', hist_bins: int=50) -> None:
+        """ Plots a histogram and QQ plot of the backtested returns distribution
+
+        Args:
+            title (str, optional): title of plots. Defaults to 'Backtest Returns Distribution'.
+            hist_bins (int, optional): number of bins for histogram. Defaults to 50.
+        """
         self._check_backtest_ran
         plot_returns_distribution(
             self.portfolio_returns,
